26867a7da8b5038657c45a45a4914ba9ec013beb
Rana Salem <ranatasalem@gmail.com>
2024-05-08 23:02:27 +0100

    update console logs

--- app/index.js
+++ app/index.js
@@ -20,16 +20,16 @@ const bgClient = new BusgresClient(sbConnectionString, sbConfig, pgConfig)
 bgClient
   .connect()
   .then(async () => {
-    console.log('Connected to PostgreSQL database')
+    console.log(`You are now connected to the ${process.env.DATABASE} database in PostgreSQL.`)
 
-    const query = 'SELECT * FROM busgres'
+    const query = 'select * from busgres'
     const result = await bgClient.pgClient.query(query)
 
     console.log('All messages in the database:')
     result.rows.forEach((row, index) => {
-      console.log(`Message ${index + 1}:`, row)
+      console.log(`Row ${index + 1}:`, row)
     })
   })
   .catch((error) => {
-    console.error('Error connecting to PostgreSQL database:', error)
+    console.error('There has been an error connecting to your PostgreSQL database:', error)
   })
