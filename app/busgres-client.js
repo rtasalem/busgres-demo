436f5e25635890646e5d3c713ee045f836cbca72
Rana Salem <ranatasalem@gmail.com>
2025-05-23 23:15:38 +0100

    update how ServiceBusHandler is instantiated

--- app/busgres-client.js
+++ app/busgres-client.js
@@ -1,8 +1,17 @@
 import { BusgresClient } from 'busgres'
 import 'dotenv/config'
-import { config } from './config/index.js'
 
 export const busgresClient = new BusgresClient({
-  serviceBus: config.get('serviceBus'),
-  postgres: config.get('postgres')
+  serviceBus: {
+    connectionString: process.env.SB_CONNECTION_STRING,
+    entity: process.env.SB_ENTITY,
+    entityType: 'queue'
+  },
+  postgres: {
+    username: process.env.PG_USERNAME,
+    password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE,
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT
+  }
 })
